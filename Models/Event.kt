3ed6880e2df79e9d557c31859eed405f290058f7
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2025-06-01 15:46:36 +0300

    Create Event.kt

---
+++ Models/Event.kt
@@ -0,0 +1,9 @@
+data class Event(
+    val title: String = "",
+    val description: String = "",
+    val location: String = "",
+    val imageUrl: String = "",
+    val date: String = "",
+    val createdAt: String = "",
+    var id: String = ""
+)
