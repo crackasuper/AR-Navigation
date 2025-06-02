59d46d4ddc0d3ec27c277956aa5d6f492e3a25e1
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2025-06-02 21:16:15 +0300

    Create NewPlace.kt

---
+++ persistance/NewPlace.kt
@@ -0,0 +1,13 @@
+
+
+data class NewPlace(
+
+    val name: String,
+    val lat: Double,
+    val lng: Double,
+    val alt: Double,
+    val heading: Double,
+    val description: String,
+    val author: String,
+    val ardata: String
+)
