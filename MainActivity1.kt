b870084cf0d35fae6220ce9dd052a2f514fd223e
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2025-06-03 09:24:07 +0300

    Create MainActivity1.kt

---
+++ MainActivity1.kt
@@ -0,0 +1,26 @@
+
+
+
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+
+class MainActivity1: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val authViewModel: AuthViewModel by viewModels()
+        setContent{
+
+            MyApplicationNav(modifier = Modifier.padding(),authViewModel = authViewModel)
+
+        }
+    }
+
+}
